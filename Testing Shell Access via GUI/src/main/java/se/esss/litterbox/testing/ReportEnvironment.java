cd12ab3e924df0a3fdd1d0117d580798ad5b6233
stevemolloy <stephen.molloy@ice-tech.systems>
2016-03-21 18:32:26 +0100

    First commit

---
+++ Testing Shell Access via GUI/src/main/java/se/esss/litterbox/testing/ReportEnvironment.java
@@ -0,0 +1,43 @@
+package se.esss.litterbox.testing;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class ReportEnvironment {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ReportEnvironment window = new ReportEnvironment();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ReportEnvironment() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
